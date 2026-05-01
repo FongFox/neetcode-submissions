4502e2477088dbe9f69b6895f38bf6760acc20db
Tran Gia Nguyen Phong <107380312+FongFox@users.noreply.github.com>
2026-05-01 19:56:38 +0700

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        if(nums.length < 1) {
+            return false;
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+
+        return false;
+    }
+}
