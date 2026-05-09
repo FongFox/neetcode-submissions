5259185d55538f5dd7687372d1e16aed3a025226
Tran Gia Nguyen Phong <107380312+FongFox@users.noreply.github.com>
2026-05-09 21:37:55 +0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length() < 1) {
+            return false;
+        }
+
+        int n = s.length();
+        int leftPtr = 0;
+        int rightPtr = n - 1;
+        while(leftPtr < rightPtr) {
+            if(!Character.isLetterOrDigit(s.charAt(leftPtr))) {
+                leftPtr++;
+                continue;
+            }
+
+            if(!Character.isLetterOrDigit(s.charAt(rightPtr))) {
+                rightPtr--;
+                continue;
+            }
+            
+            if(Character.toLowerCase(s.charAt(leftPtr++)) != Character.toLowerCase(s.charAt(rightPtr--))) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
